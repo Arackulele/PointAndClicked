2c13fac745fffbbcaca9ad233367b2d771424b09
Ara Arackulele <nicki.walter@web.de>
2023-04-06 10:21:08 +0200

    minor additions

---
+++ Assets/Quest.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quest : Object
+{
+    // Classes: Standart, Hat, Shirt, Trousers, Boots, Firearms
+    public string name;
+    public string description;
+    public string additional_info;
+    public string task;
+
+    void Start(string pName, string pDesc, string pClass) {
+        
+    }
+
+    void Update() {
+
+    }
+}
